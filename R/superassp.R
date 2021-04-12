f1e6ed62f2468ccf539331e366abc5d5ae77e804
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2021-04-12 10:17:51 +0200

    New package dockumentation, which also incorporates the loading of Rd citation code.

---
+++ R/superassp.R
@@ -0,0 +1,18 @@
+
+#' superassp: Praat, MATLAB(TM) and wrassp speech signal processing using a wrassp-like interface
+#'
+#' This package bundles together routines that utilizes Praat, matlab or python
+#' to do the signal processing. The output of the original routines are then
+#' wrapped into a common interface that outputs an SSFF file that is compatible
+#' with the output of the wrassp package functions. This package also re-exports
+#' all of wrassp's functions so it is usually enough to just load superassp.
+#' 
+#'
+#' @docType package
+#' @name superassp
+#' @importFrom Rdpack reprompt
+#' 
+NULL
+#> NULL
+
+PRAAT_DEVEL = FALSE
