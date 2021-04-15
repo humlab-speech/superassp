7bc2f29982f5e66c30601a53e0f0294b8681e420
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2021-04-15 15:11:26 +0200

    New get_extension function that unifies the retrieval of file extension from a function or file

--- R/trackdata_extensions.R
+++ R/trackdata_extensions.R
@@ -36,3 +36,42 @@ get_definedtracks <- function(x){
   }
   
 }
+
+#' Get the (default) extension for an SSFF producing function or a signal file 
+#'
+#'
+#' @param x The name of function defined to output trackdata, or the full path to a file that can be read into a [wrassp::AsspDataObj].
+#'
+#' @return A string indicating the default file extension of the SSFF generating function, or the file extension of the signal file.
+#' @export
+#'
+#' @examples
+#' get_extension("forest")
+#' get_extension("praat_formant_burg")
+#'
+
+get_extension <- function(x){
+  
+  if(file.exists(x)){
+    tryCatch({
+      read.AsspDataObj(x) -> r
+    },error=function(e) {stop("Could not open the file ",x," for reading. Please ensure that it is an SSFF file!")})
+    return(tools::file_ext(r))
+  }
+  
+  if(exists(x) & is.function(get(x))){
+    
+    fun <- get(x)
+    #Check that the function has been prepared for use with this function by
+    # giving it the the required additional attributes "ext" and "tracks"
+    if(!is.null(attr(fun,"ext")) ){
+      return(attr(fun,"ext"))
+    }
+  }
+  
+  if( is.null(x) || !is.null(wrassp::wrasspOutputInfos[[x]])){
+    #Wrassp function
+    return(wrassp::wrasspOutputInfos[[x]][["ext"]])
+  }
+  
+}
