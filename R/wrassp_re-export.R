efb503377fc390591ac07a6e0ac550390f054031
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2021-05-10 14:03:11 +0200

    Re-export functions from wrassp for more unified and convenient access.

---
+++ R/wrassp_re-export.R
@@ -0,0 +1,90 @@
+#' Formant estimation (From the 'wrassp' package)
+#' @export
+#' @inheritParams wrassp::forest
+#' @inherit wrassp::forest return references description details sections seealso
+#' @seealso [wrassp::forest]
+forest = wrassp::forest
+
+#' Analysis of short-term autocorrelation function (From the 'wrassp' package)
+#' @export
+#' @inheritParams wrassp::acfana
+#' @inherit wrassp::acfana return references description details sections seealso
+#' @seealso [wrassp::acfana]
+acfana = wrassp::acfana
+
+#' Computes the first difference of the signal (From the 'wrassp' package)
+#' @export
+#' @inheritParams wrassp::afdiff
+#' @inherit wrassp::afdiff return references description details sections seealso
+#' @seealso [wrassp::afdiff]
+afdiff = wrassp::afdiff
+
+#' Filters the audio signal (From the 'wrassp' package)
+#' @export
+#' @inheritParams wrassp::affilter
+#' @inherit wrassp::affilter return references description details sections seealso
+#' @seealso [wrassp::affilter]
+affilter = wrassp::affilter
+
+#' Short-term cepstral analysis (From the 'wrassp' package)
+#' @export
+#' @inheritParams wrassp::cepstrum
+#' @inherit wrassp::cepstrum return references description details sections seealso
+#' @seealso [wrassp::cepstrum]
+cepstrum = wrassp::cepstrum
+
+#' Cepstral smoothed version of 'dftSpectrum' (From the 'wrassp' package)
+#' @export
+#' @inheritParams wrassp::cssSpectrum
+#' @inherit wrassp::cssSpectrum return references description details sections seealso
+#' @seealso [wrassp::cssSpectrum]
+cssSpectrum = wrassp::cssSpectrum
+
+#' Short-term DFT spectral analysis (From the 'wrassp' package)
+#' @export
+#' @inheritParams wrassp::dftSpectrum
+#' @inherit wrassp::dftSpectrum return references description details sections seealso
+#' @seealso [wrassp::dftSpectrum]
+dftSpectrum = wrassp::dftSpectrum
+
+#' F0 analysis of the signal (From the 'wrassp' package)
+#' @export
+#' @inheritParams wrassp::ksvF0
+#' @inherit wrassp::ksvF0 return references description details sections seealso
+#' @seealso [wrassp::ksvF0]
+ksvF0 = wrassp::ksvF0
+
+#' Linear Predictive smoothed version of 'dftSpectrum' (From the 'wrassp' package)
+#' @export
+#' @inheritParams wrassp::lpsSpectrum
+#' @inherit wrassp::lpsSpectrum return references description details sections seealso
+#' @seealso [wrassp::lpsSpectrum]
+lpsSpectrum = wrassp::lpsSpectrum
+
+#' Pitch analysis of the speech signal using Michel's (M)odified (H)armonic (S)ieve algorithm (From the 'wrassp' package)
+#' @export
+#' @inheritParams wrassp::mhsF0
+#' @inherit wrassp::mhsF0 return references description details sections seealso
+#' @seealso [wrassp::mhsF0]
+mhsF0 = wrassp::mhsF0
+
+#' Linear Prediction analysis (From the 'wrassp' package)
+#' @export
+#' @inheritParams wrassp::rfcana
+#' @inherit wrassp::rfcana return references description details sections seealso
+#' @seealso [wrassp::rfcana]
+rfcana = wrassp::rfcana
+
+#' Analysis of short-term Root Mean Square amplitude (From the 'wrassp' package)
+#' @export
+#' @inheritParams wrassp::rmsana
+#' @inherit wrassp::rmsana return references description details sections seealso
+#' @seealso [wrassp::rmsana]
+rmsana = wrassp::rmsana
+
+#' Analysis of the averages of the short-term positive and negative zero-crossing rates (From the 'wrassp' package)
+#' @export
+#' @inheritParams wrassp::zcrana
+#' @inherit wrassp::zcrana return references description details sections seealso
+#' @seealso [wrassp::zcrana]
+rmsana = wrassp::zcrana
