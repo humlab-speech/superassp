dde393c3a1a9f916b8158be445e2c8a586c80910
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2021-08-16 13:43:50 +0200

    New consistent testing for all praat functions.

--- tests/testthat/test_praat.R
+++ tests/testthat/test_praat.R
@@ -4,12 +4,23 @@ library(testthat)
 
 testFile <- file.path("..","signalfiles","msajc003.wav")
 
-test_that("Praat_formant_burg can generate signal files",{
-  ssff <- praat_formant_burg(testFile,toFile=FALSE)
+praat_funs <- c("praat_formant_burg", "praat_intensity","praat_moments","praat_sauce")
 
-  expect_true(base::setequal(names(ssff),c("fm","bw")))
-  
-  
-  
-  
-})
+for(f in praat_funs){
+  test_that(paste("Confirm that",f,"can generate valid SSFF files"),{
+    ssff <- do.call(f,list(testFile,toFile=FALSE))
+    
+    ext <- superassp::get_extension(f)
+    tracks <- superassp::get_definedtracks(f)
+    
+    expect_true(base::setequal(names(ssff),tracks))
+    
+    tf <- tempfile(fileext = ext)
+    wrassp::write.AsspDataObj(ssff,file = tf)
+    tfRead <- wrassp::read.AsspDataObj(fname=tf)
+    
+    expect_true(base::setequal(names(tfRead),tracks))
+    
+  })
+}
+
