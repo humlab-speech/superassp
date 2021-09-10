c7377a374204d73f9d236c3d15cfd31b585cf718
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2021-09-10 14:36:42 +0200

    New function to compute a Dysphonia Severity Index for a speaker based on a set of voice samples where different types of voice excursions are performed.

--- R/praat_slicefunctions.R
+++ R/praat_slicefunctions.R
@@ -63,6 +63,8 @@
 #' \item{AVQI}{Acoustic Voice Quality Index summarizing the measures above}
 #' }
 #' 
+#' @export
+#' 
 #' @references
 #'  \insertAllCited{}
 
@@ -349,10 +351,246 @@ praat_voice_report <- function(filename,
 }
 attr(praat_voice_report,"outputType") <-  c("list")
 
-# FOR INTERACTIVE TESTING
-# df <- data.frame("absolute_file_path"="~/Desktop/kaa_yw_pb.wav","start"=0,"end"=3)
-# df2 <- data.frame("absolute_file_path"=c("~/Desktop/kaa_yw_pb.wav","~/Desktop/kaa_yw_pb.wav"),"start"=c(0,0),"end"=c(3,3))
-# 
-# praat_avqi(df, df,pdf.path="~/Desktop/test/",simple.output = TRUE) -> outTab
 
+
+
+#' Compute the Dysphonia Severity Index
+#'
+#' This function computes the Dysphonia Severity Index (DSI)
+#' \insertCite{Wuyts:2000vb}{superassp} as implemented into Praat by
+#' \insertCite{Maryn.2017.10.1016/j.jvoice.2017.01.002;textual}{superassp}. The
+#' user is asked to supply data.frames indicating the "absolute_file_path" as
+#' well as "start", and "end" times for parts of sound files that together form
+#' the basis for the DSI computation for a speaker.
+#'
+#' The user has to indicate at least one sample in which the participant speaks
+#' as softly as possible (`softDF`), at least one sample from which the maximum
+#' f$_0$ could be deduced (`highpitchDF`), at least one sample where a single
+#' vowel is maximally prolonged (`maxprolongedDF`). The user can also provide a
+#' sample of a maximally stable vowel (`stableDF`), but if such a sample is not
+#' provided then the (`maxprolongedDF`) sample will be reused instead for the
+#' computation of the DSI sub-component Jitter. The user may submit multiple
+#' sound samples for all these sets of acoustic inputs, and the sounds files
+#' will then be combined before the DSI sub components are computed. The largest
+#' Maximum performance time will be used.
+#'
+#'
+#' @param softDF A data.frame containing the file path and start and end times
+#'   of samples indicating the softest voice production of the speaker.
+#' @param highpitchDF A data.frame containing the file path and start and end
+#'   times of samples from which the highest f0 of the speaker can be computed.
+#' @param maxprolongedDF A data.frame containing the file path and start and end
+#'   times of samples indicating maximally prolonged vowels of the speaker.
+#' @param stableDF An optional data.frame containing the file path and start and
+#'   end times of samples indicating a stable sustained vowel. If not provided,
+#'   the `maxprolongedDF` will be used instead.
+#' @param use.calibration Should a calibration factor be added to measured
+#'   intensities before computing DSI?
+#' @param db.calibration The number of dB:s to be added to measured intensity
+#'   values before computing DSI.
+#' @param speaker.name An optional name of the speaker. Only used when PDF
+#'   output is produced.
+#' @param speaker.ID An ID of the speaker. This will be included in the list
+#'   output.
+#' @param speaker.dob An optional date of birth of the speaker. Only used when
+#'   PDF output is produced.
+#' @param session.datetime A string indicating the session date (and time). Only
+#'   used when PDF output is produced.
+#' @param pdf.path A path where a PDF report file will be produced.
+#' @param overwrite.pdfs Should existing PDF files be overwritten in the PDF
+#'   output directory? Defaults to a safe behavior where older PDFs are not
+#'   overwritten.
+#' @param praat_path An explicit path to the Praat binary.
+#'
+#' @return A list with the following fields: 
+#' \describe{ 
+#' \item{ID}{The speaker speaker + session identifier of the output}
+#'   \item{Maximum.phonation.time}{The speakers maximum phonation time.}
+#'   \item{Softest.intensity.of.voiced.speech}{The intensity of the participants
+#'   softest voice (in dB)} 
+#'   \item{Maximum.fundamental.frequency}{The maximum f0
+#'   frequency (in Hz)} 
+#'   \item{Jitter.ppq5}{The five-point Period Pertubation
+#'   Quotient measurement (in \%)} 
+#'   \item{Dysphonia.Severity.Index}{The resulting
+#'   Dysphonia Severity Index } 
+#'   }
+#'
+#'   A meta-analysis in which 1330 healthy participants (aged 17.3−94) were included indicated an average DSI of 3.05 in healthy speakers, with a conﬁdence level  between
+#'   2.13 and 3.98 \insertCite{Sobol.2020.10.1016/j.jvoice.2020.04.010}{superassp}. 
+#'
+#'
+#' @export
+#'
+#' @references 
+#' \insertAllCited{}
+#' 
+
+praat_dsi <- function(softDF,
+                      highpitchDF,
+                      maxprolongedDF,
+                      stableDF=NULL,
+                      use.calibration=FALSE,
+                      db.calibration=10,
+                      speaker.name=NULL,
+                      speaker.ID=NULL,
+                      speaker.dob=NULL,
+                      session.datetime=NULL,
+                      pdf.path=NULL,
+                      overwrite.pdfs=FALSE,
+                      praat_path=NULL){
+  
+  
+  requiredDFColumns <- c("absolute_file_path","start","end")
+  # Reuse max prolonged vowels as stable productions.
+  
+  if(is.null(stableDF) ) {
+    stableDF <- maxprolongedDF
+  }
+  if(nrow(softDF) < 1 || nrow(highpitchDF) < 1 || nrow(maxprolongedDF) < 1 || nrow(stableDF) < 1 ){
+    stop("All of the supplied dataframes indicating samples to be analysed must be non-empty.")
+  }
+  
+  listOfFiles <- unique(
+    c(softDF$absolute_file_path,
+      highpitchDF$absolute_file_path,
+      maxprolongedDF$absolute_file_path,
+      stableDF$absolute_file_path)
+  )
+  
+  if(! have_praat(praat_path)){
+    stop("Could not find praat. Please specify a full path.")
+  }
+  
+
+  if(! requiredDFColumns %in% names(softDF) || 
+     ! requiredDFColumns %in% names(highpitchDF)  || 
+     ! requiredDFColumns %in% names(maxprolongedDF) || 
+     ! requiredDFColumns %in% names(stableDF)   
+  ){
+    stop("All dataframes must both contain columns named ",paste(requiredDFColumns,collapse=",",sep=""),".")
+  }
+  
+  praat_script <- ifelse(dir.exists("inst"), ## This means that we are developing
+                         file.path("inst","praat","DSI201.praat"),
+                         file.path(system.file(package = "superassp",mustWork = TRUE),"praat","DSI201.praat"))
+  
+
+  dsi <- tjm.praat::wrap_praat_script(praat_location = get_praat(),
+                                       script_code_to_run = readLines(praat_script)
+                                       ,return="last-argument")
+  
+  #Check that all files exists before we begin
+  filesEx <- file.exists(listOfFiles)
+  if(!all(filesEx)){
+    filedNotExists <- listOfFiles[!filesEx]
+    stop("Unable to find the sound file(s) ",paste(filedNotExists, collapse = ", "))
+  }
+  
+  # Set up a (CLEAN) directory for interchange
+  dsiDir <- file.path(tempdir(check=TRUE),"dsitemp")
+  unlink(dsiDir,recursive = TRUE,force=FALSE,expand=FALSE)
+  dir.create(dsiDir)
+  
+  
+  #The empty vector of file names that should be returned
+  outListOfFiles <- c()
+
+  #Copy soft voice samples
+  
+  #Pre-generate names of output files
+  softDF$OutFileName <- file.path(dsiDir,paste0("im",1:nrow(softDF),".wav"))
+  
+  for(r in 1:nrow(softDF)){
+    #Here we finally copy out all signal file content into separate files
+    currSound <- wrassp::read.AsspDataObj(fname=softDF[r,"absolute_file_path"],begin=softDF[r,"start"],end=softDF[r,"end"])
+    wrassp::write.AsspDataObj(currSound,file=softDF[r,"OutFileName"])
+  }
+  
+  #Copy highest pitch samples
+  
+  #Pre-generate names of output files
+  highpitchDF$OutFileName <- file.path(dsiDir,paste0("fh",1:nrow(highpitchDF),".wav"))
+  
+  for(r in 1:nrow(highpitchDF)){
+    #Here we finally copy out all signal file content into separate files
+    currSound <- wrassp::read.AsspDataObj(fname=highpitchDF[r,"absolute_file_path"],begin=highpitchDF[r,"start"],end=highpitchDF[r,"end"])
+    wrassp::write.AsspDataObj(currSound,file=highpitchDF[r,"OutFileName"])
+  }
+  
+  #Copy maximally prolonged vowel samples
+  
+  #Pre-generate names of output files
+  maxprolongedDF$OutFileName <- file.path(dsiDir,paste0("mpt",1:nrow(maxprolongedDF),".wav"))
+  
+  for(r in 1:nrow(maxprolongedDF)){
+    #Here we finally copy out all signal file content into separate files
+    currSound <- wrassp::read.AsspDataObj(fname=maxprolongedDF[r,"absolute_file_path"],begin=maxprolongedDF[r,"start"],end=maxprolongedDF[r,"end"])
+    wrassp::write.AsspDataObj(currSound,file=maxprolongedDF[r,"OutFileName"])
+  }
+  
+  #Copy stable vowel samples
+  
+  #Pre-generate names of output files
+  stableDF$OutFileName <- file.path(dsiDir,paste0("ppq",1:nrow(stableDF),".wav"))
+  
+  for(r in 1:nrow(stableDF)){
+    #Here we finally copy out all signal file content into separate files
+    currSound <- wrassp::read.AsspDataObj(fname=stableDF[r,"absolute_file_path"],begin=stableDF[r,"start"],end=stableDF[r,"end"])
+    wrassp::write.AsspDataObj(currSound,file=stableDF[r,"OutFileName"])
+  }
+  
+  
+  # Now we are all set up to run the Praat script
+  # boolean Apply_calibration 1
+  # # sentence Calibration_factor 1*self+10
+  # sentence Name_of_patient Fredrik Karlsson
+  # sentence Date_of_birth 1975-12-31
+  # sentence Assessment_date 2021-12-31
+  # sentence Input_directory ../../tests/signalfiles/DSI/input
+  # #/Users/frkkan96/Documents/src/superassp/tests/signalfiles/DSI/input
+  # boolean Generate_PDF_files 1
+  # sentence Speaker_ID 1
+  # sentence Output_directory /Users/frkkan96/Documents/src/superassp/tests/signalfiles/DSI/output
+  # sentence Output_file /Users/frkkan96/Documents/src/superassp/tests/signalfiles/DSI/output/dsi.csv
+  
+  outDSITabFile <- dsi(ifelse(use.calibration,1,0),
+                       db.calibration,
+                       ifelse(! is.null(speaker.name),shQuote(speaker.name),"NA"),
+                       ifelse(! is.null(speaker.dob),shQuote(speaker.dob),"NA"),
+                       ifelse(! is.null(session.datetime),shQuote(session.datetime),"NA"),
+                       dsiDir,
+                       ifelse(! is.null(pdf.path),1,0),
+                       ifelse(! is.null(speaker.ID),speaker.ID,"NA"),
+                       dsiDir,
+                       file.path(dsiDir,"dsi.csv")
+  )
+  
+  inTable <- read.csv(file=outDSITabFile
+                      ,header=TRUE
+                      ,na.strings =c("--undefined--","NA"),
+                      sep = ",")
+  
+  assertthat::are_equal(nrow(inTable),1)
+  
+  # Now copy PDF files to the pdf.output path
+  if(!is.null(pdf.path)){
+    pdfFiles <- list.files(dsiDir,pattern=".*[.]pdf",full.names=TRUE)
+    for(currFile in pdfFiles){
+      file.copy(from=currFile,to=pdf.path,overwrite=overwrite.pdfs)
+    }
+  }
+  return(as.list(inTable))  
+}
+attr(praat_dsi,"outputType") <-  c("list")
+
+
+
+# FOR INTERACTIVE TESTING
+#df <- data.frame("absolute_file_path"="~/Desktop/kaa_yw_pb.wav","start"=0,"end"=3)
+#df2 <- data.frame("absolute_file_path"=c("~/Desktop/kaa_yw_pb.wav","~/Desktop/kaa_yw_pb.wav"),"start"=c(0,0),"end"=c(3,3))
+#  
+#praat_avqi(df, df,pdf.path="~/Desktop/test/",simple.output = TRUE) -> out
+#praat_dsi(df, df,df2,pdf.path="~/Desktop/test/") -> out
+# praat_dsi(, df,df2,pdf.path="~/Desktop/test/") -> out
 #praat_voice_report("~/Desktop/aaa_sample.wav") -> out
