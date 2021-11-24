c8d8cec3c8065274283d3a100ba31b01cc52b37e
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2021-11-24 13:00:52 +0100

    Added attributes to all exported wrassp DSP functions, negating the requirement to go to wrasspOutputInfos to find the info.

--- R/trackdata_extensions.R
+++ R/trackdata_extensions.R
@@ -1,7 +1,7 @@
 #' Get available tracks for a function or an SSFF file
 #'
 #'
-#' @param x The name of function defined to output trackdata, or the full path to a file that can be read into a [wrassp::AsspDataObj].
+#' @param x The name of function defined to output trackdata, or the full path to a file that can be read using [wrassp::read.AsspDataObj].
 #'
 #' @return A vector of tracks that the function is defined to return, or are contained within the file.
 #' @export
@@ -10,9 +10,9 @@
 #' get_definedtracks("forest")
 #' get_definedtracks("praat_formant_burg")
 #'
-get_definedtracks <- function(x){
+get_definedtracks <- function(x,package="superassp"){
   
-  if(file.exists(x)){
+  if(is.character(x) && file.exists(x)){
     tryCatch({
       read.AsspDataObj(x) -> r
       tr <- tracks.AsspDataObj(r)
@@ -20,33 +20,26 @@ get_definedtracks <- function(x){
     return(tr)
   }
   
-  if( is.null(x) || !is.null(wrassp::wrasspOutputInfos[[x]])){
-    #Wrassp function
-    return(wrassp::wrasspOutputInfos[[x]][["tracks"]])
-  } else {
-    if(exists(x) && is.function(get(x))){
-      
-      fun <- get(x)
-      #Check that the function has been prepared for use with this function by
-      # giving it the the required additional attributes "ext" and "tracks"
-      if(is.null(attr(fun,"tracks")) ){
-        
-        warning("The function ",onTheFlyFunctionName," is not defined correctly. NULL is returned. \nPlease provide it with the attributes \"ext\" and \"tracks\".\n See ?attr for details, as well as attributes(praat_formant_burg) for an example." )
-        
-      }
-      return(attr(fun,"tracks"))
-    }
+  if(is.character(x)){
+    fun <- utils::getFromNamespace(x,package)
+  }else{
+    fun <- x
   }
-
   
-
-  
+  #Check that the function has been prepared for use with this function by
+  # giving it the the required additional attributes "ext" and "tracks"
+  if(is.null(attr(fun,"tracks")) ){
+    
+    stop("The function ",x," is not defined correctly.\nPlease provide attributes \"tracks\" for the function.\n See ?attr for details, as well as attributes(praat_formant_burg) for an example." )
+    
+  }
+  return(attr(fun,"tracks"))
 }
 
 #' Get the (default) extension for an SSFF producing function or a signal file 
 #'
 #'
-#' @param x The name of function defined to output trackdata, or the full path to a file that can be read into a [wrassp::AsspDataObj].
+#' @param x The name of function defined to output trackdata, or the full path to a file that can be read using [wrassp::read.AsspDataObj].
 #'
 #' @return A string indicating the default file extension of the SSFF generating function, or the file extension of the signal file.
 #' @export
@@ -56,28 +49,64 @@ get_definedtracks <- function(x){
 #' get_extension("praat_formant_burg")
 #'
 
-get_extension <- function(x){
+get_extension <- function(x,package="superassp"){
   
-  if(file.exists(x)){
+  if(is.character(x) && file.exists(x)){
     tryCatch({
-      read.AsspDataObj(x) -> r
+      read.AsspDataObj(x,begin=0, end=1, samples = TRUE) -> r
     },error=function(e) {stop("Could not open the file ",x," for reading. Please ensure that it is an SSFF file!")})
-    return(tools::file_ext(r))
+    return(tools::file_ext(x))
   }
   
-  if(exists(x) & is.function(get(x))){
+  if(is.character(x)){
+    fun <- utils::getFromNamespace(x,package)
+  }else{
+    fun <- x
+  }
+  
+  #Check that the function has been prepared for use with this function by
+  # giving it the the required additional attributes "ext"
+  if(is.null(attr(fun,"ext")) ){
     
-    fun <- get(x)
-    #Check that the function has been prepared for use with this function by
-    # giving it the the required additional attributes "ext" and "tracks"
-    if(!is.null(attr(fun,"ext")) ){
-      return(attr(fun,"ext"))
-    }
+    stop("The function ",x," is not defined correctly.\nPlease provide attributes \"ext\" for the function.\n See ?attr for details, as well as attributes(praat_formant_burg) for an example." )
+    
   }
-  
-  if( is.null(x) || !is.null(wrassp::wrasspOutputInfos[[x]])){
-    #Wrassp function
-    return(wrassp::wrasspOutputInfos[[x]][["ext"]])
-  }
+  return(attr(fun,"ext"))
   
 }
+
+
+#' Get the return format of a wrassp/superassp speech signal processing function 
+#'
+#' Speech signal processing functions in superassp may return an SSFF track with one or more columns, or a simple, not nested list. This function may be used to learn the output type of a specific function. 
+#' @param x The name of a speech signal processing function that are defined in the superassp or wrassp packages.
+#' @param package The name of the package where the function is defined.
+#'
+#' @return Either "SSFF" or "list".
+#' @export
+#'
+#' @examples
+#' get_outputType("forest")
+#' get_outputType("praat_avqi")
+#' get_outputType("praat_formant_burg")
+
+get_outputType <- function(x,package="superassp"){
+  
+  
+  if(is.character(x)){
+    fun <- utils::getFromNamespace(x,package)
+  }else{
+    fun <- x
+  }
+ 
+  #Check that the function has been prepared for use with this function by
+  # giving it the the required additional attributes "ext" and "tracks"
+  if(is.null(attr(fun,"outputType")) ){
+    
+    stop("The function ",x," is not defined correctly. See ?attr for details, as well as attributes(superassp::praat_formant_burg) for an example." )
+    
+  }
+  return(attr(fun,"outputType"))
+}
+  
+
--- R/wrassp_re-export.R
+++ R/wrassp_re-export.R
@@ -4,6 +4,10 @@
 #' @inherit wrassp::forest return references description details sections seealso
 #' @seealso [wrassp::forest]
 forest = wrassp::forest
+attr(forest,"ext") <-  wrassp::wrasspOutputInfos[["forest"]]$ext 
+attr(forest,"tracks") <-  wrassp::wrasspOutputInfos[["forest"]]$tracks
+attr(forest,"outputType") <-  wrassp::wrasspOutputInfos[["forest"]]$outputType
+
 
 #' Analysis of short-term autocorrelation function (From the 'wrassp' package)
 #' @export
@@ -11,6 +15,9 @@ forest = wrassp::forest
 #' @inherit wrassp::acfana return references description details sections seealso
 #' @seealso [wrassp::acfana]
 acfana = wrassp::acfana
+attr(acfana,"ext") <-  wrassp::wrasspOutputInfos[["acfana"]]$ext 
+attr(acfana,"tracks") <-  wrassp::wrasspOutputInfos[["acfana"]]$tracks
+attr(acfana,"outputType") <-  wrassp::wrasspOutputInfos[["acfana"]]$outputType
 
 #' Computes the first difference of the signal (From the 'wrassp' package)
 #' @export
@@ -18,6 +25,10 @@ acfana = wrassp::acfana
 #' @inherit wrassp::afdiff return references description details sections seealso
 #' @seealso [wrassp::afdiff]
 afdiff = wrassp::afdiff
+attr(afdiff,"ext") <-  wrassp::wrasspOutputInfos[["afdiff"]]$ext 
+attr(afdiff,"tracks") <-  wrassp::wrasspOutputInfos[["afdiff"]]$tracks
+attr(afdiff,"outputType") <-  wrassp::wrasspOutputInfos[["afdiff"]]$outputType
+
 
 #' Filters the audio signal (From the 'wrassp' package)
 #' @export
@@ -25,6 +36,9 @@ afdiff = wrassp::afdiff
 #' @inherit wrassp::affilter return references description details sections seealso
 #' @seealso [wrassp::affilter]
 affilter = wrassp::affilter
+attr(affilter,"ext") <-  wrassp::wrasspOutputInfos[["affilter"]]$ext 
+attr(affilter,"tracks") <-  wrassp::wrasspOutputInfos[["affilter"]]$tracks
+attr(affilter,"outputType") <-  wrassp::wrasspOutputInfos[["affilter"]]$outputType
 
 #' Short-term cepstral analysis (From the 'wrassp' package)
 #' @export
@@ -32,6 +46,9 @@ affilter = wrassp::affilter
 #' @inherit wrassp::cepstrum return references description details sections seealso
 #' @seealso [wrassp::cepstrum]
 cepstrum = wrassp::cepstrum
+attr(cepstrum,"ext") <-  wrassp::wrasspOutputInfos[["cepstrum"]]$ext 
+attr(cepstrum,"tracks") <-  wrassp::wrasspOutputInfos[["cepstrum"]]$tracks
+attr(cepstrum,"outputType") <-  wrassp::wrasspOutputInfos[["cepstrum"]]$outputType
 
 #' Cepstral smoothed version of 'dftSpectrum' (From the 'wrassp' package)
 #' @export
@@ -39,6 +56,9 @@ cepstrum = wrassp::cepstrum
 #' @inherit wrassp::cssSpectrum return references description details sections seealso
 #' @seealso [wrassp::cssSpectrum]
 cssSpectrum = wrassp::cssSpectrum
+attr(cssSpectrum,"ext") <-  wrassp::wrasspOutputInfos[["cssSpectrum"]]$ext 
+attr(cssSpectrum,"tracks") <-  wrassp::wrasspOutputInfos[["cssSpectrum"]]$tracks
+attr(cssSpectrum,"outputType") <-  wrassp::wrasspOutputInfos[["cssSpectrum"]]$outputType
 
 #' Short-term DFT spectral analysis (From the 'wrassp' package)
 #' @export
@@ -46,6 +66,9 @@ cssSpectrum = wrassp::cssSpectrum
 #' @inherit wrassp::dftSpectrum return references description details sections seealso
 #' @seealso [wrassp::dftSpectrum]
 dftSpectrum = wrassp::dftSpectrum
+attr(dftSpectrum,"ext") <-  wrassp::wrasspOutputInfos[["dftSpectrum"]]$ext 
+attr(dftSpectrum,"tracks") <-  wrassp::wrasspOutputInfos[["dftSpectrum"]]$tracks
+attr(dftSpectrum,"outputType") <-  wrassp::wrasspOutputInfos[["dftSpectrum"]]$outputType
 
 #' F0 analysis of the signal (From the 'wrassp' package)
 #' @export
@@ -53,6 +76,9 @@ dftSpectrum = wrassp::dftSpectrum
 #' @inherit wrassp::ksvF0 return references description details sections seealso
 #' @seealso [wrassp::ksvF0]
 ksvF0 = wrassp::ksvF0
+attr(ksvF0,"ext") <-  wrassp::wrasspOutputInfos[["ksvF0"]]$ext 
+attr(ksvF0,"tracks") <-  wrassp::wrasspOutputInfos[["ksvF0"]]$tracks
+attr(ksvF0,"outputType") <-  wrassp::wrasspOutputInfos[["ksvF0"]]$outputType
 
 #' Linear Predictive smoothed version of 'dftSpectrum' (From the 'wrassp' package)
 #' @export
@@ -60,6 +86,9 @@ ksvF0 = wrassp::ksvF0
 #' @inherit wrassp::lpsSpectrum return references description details sections seealso
 #' @seealso [wrassp::lpsSpectrum]
 lpsSpectrum = wrassp::lpsSpectrum
+attr(lpsSpectrum,"ext") <-  wrassp::wrasspOutputInfos[["lpsSpectrum"]]$ext 
+attr(lpsSpectrum,"tracks") <-  wrassp::wrasspOutputInfos[["lpsSpectrum"]]$tracks
+attr(lpsSpectrum,"outputType") <-  wrassp::wrasspOutputInfos[["lpsSpectrum"]]$outputType
 
 #' Pitch analysis of the speech signal using Michel's (M)odified (H)armonic (S)ieve algorithm (From the 'wrassp' package)
 #' @export
@@ -67,6 +96,10 @@ lpsSpectrum = wrassp::lpsSpectrum
 #' @inherit wrassp::mhsF0 return references description details sections seealso
 #' @seealso [wrassp::mhsF0]
 mhsF0 = wrassp::mhsF0
+attr(mhsF0,"ext") <-  wrassp::wrasspOutputInfos[["mhsF0"]]$ext 
+attr(mhsF0,"tracks") <-  wrassp::wrasspOutputInfos[["mhsF0"]]$tracks
+attr(mhsF0,"outputType") <-  wrassp::wrasspOutputInfos[["mhsF0"]]$outputType
+
 
 #' Linear Prediction analysis (From the 'wrassp' package)
 #' @export
@@ -74,6 +107,9 @@ mhsF0 = wrassp::mhsF0
 #' @inherit wrassp::rfcana return references description details sections seealso
 #' @seealso [wrassp::rfcana]
 rfcana = wrassp::rfcana
+attr(rfcana,"ext") <-  wrassp::wrasspOutputInfos[["rfcana"]]$ext 
+attr(rfcana,"tracks") <-  wrassp::wrasspOutputInfos[["rfcana"]]$tracks
+attr(rfcana,"outputType") <-  wrassp::wrasspOutputInfos[["rfcana"]]$outputType
 
 #' Analysis of short-term Root Mean Square amplitude (From the 'wrassp' package)
 #' @export
@@ -81,6 +117,9 @@ rfcana = wrassp::rfcana
 #' @inherit wrassp::rmsana return references description details sections seealso
 #' @seealso [wrassp::rmsana]
 rmsana = wrassp::rmsana
+attr(rmsana,"ext") <-  wrassp::wrasspOutputInfos[["rmsana"]]$ext 
+attr(rmsana,"tracks") <-  wrassp::wrasspOutputInfos[["rmsana"]]$tracks
+attr(rmsana,"outputType") <-  wrassp::wrasspOutputInfos[["rmsana"]]$outputType
 
 #' Analysis of the averages of the short-term positive and negative zero-crossing rates (From the 'wrassp' package)
 #' @export
@@ -88,3 +127,6 @@ rmsana = wrassp::rmsana
 #' @inherit wrassp::zcrana return references description details sections seealso
 #' @seealso [wrassp::zcrana]
 zcrana = wrassp::zcrana
+attr(zcrana,"ext") <-  wrassp::wrasspOutputInfos[["zcrana"]]$ext 
+attr(zcrana,"tracks") <-  wrassp::wrasspOutputInfos[["zcrana"]]$tracks
+attr(zcrana,"outputType") <-  wrassp::wrasspOutputInfos[["zcrana"]]$outputType
