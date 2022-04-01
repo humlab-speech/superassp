b7e21eea9ad6357fc717ac5385359c60219f9eb3
Fredrik Karlsson <fredrik.k.karlsson@umu.se>
2022-04-01 16:22:25 +0200

    Cleanup

--- tests/testthat/test_aaa_initDemoDatabase.R
+++ tests/testthat/test_aaa_initDemoDatabase.R
@@ -1,7 +1,6 @@
 # This file is a copy of https://github.com/IPS-LMU/emuR/blob/master/tests/testthat/test_aaa_initData.R
 # for consistency
 context("Initialize data for testing")
-library(emuR)
 
 
 
