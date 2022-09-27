0cc0476e38ff58b7000724dbd43788f38be5a713
Fredrik Karlsson <dargosch@gmail.com>
2022-09-27 18:35:19 +0200

    Updates to python env setup code

--- inst/python/install_conda_env.sh
+++ inst/python/install_conda_env.sh
@@ -4,9 +4,22 @@ conda activate pysuperassp
 conda install pytorch torchaudio -c pytorch
 
 pip install https://github.com/pyannote/pyannote-audio/archive/develop.zip
-
 pip install speechbrain
-
 pip install opensmile
-
 pip install pyreaper
+pip install pysptk
+pip install librosa
+# 
+# 
+# # R code
+# library(reticulate)
+reticulate::conda_create("pysuperassp")
+reticulate::py_install("pytorch",channel="pytorch")
+reticulate::py_install("torchaudio",channel="pytorch")
+# 
+reticulate::py_install("https://github.com/pyannote/pyannote-audio/archive/develop.zip",pip=TRUE)
+reticulate::py_install("speechbrain",pip=TRUE)
+reticulate::py_install("opensmile",pip=TRUE)
+reticulate::py_install("pyreaper",pip=TRUE)
+reticulate::py_install("pysptk",pip=TRUE)
+reticulate::py_install("librosa",pip=FALSE)
