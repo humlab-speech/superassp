6a5b44fb56abc6dbc9d1388bfdc831947818a76e
Fredrik Karlsson <dargosch@gmail.com>
2023-06-15 16:41:52 +0200

    New version of voice_analysis (now voice_analysis_toolkit) that returns a slice (list)

--- R/matlab_slicefunctions.R
+++ R/matlab_slicefunctions.R
@@ -21,8 +21,7 @@ get_MatlabRuntime <- function(){
 #' The Voice Analysis Toolbox \insertCite{Tsanas:2011cb,Tsanas:2012un,tsanas2013automatic}{superassp} applies a wide range of Matlab™-implemented voice
 #' analysis procedures on a single sustained vowel (usually \[a::\]) and computes
 #' 339 acoustic quantities. This function calls a compiled application to
-#' compute the outcome measures from each sustained vowel recording in a
-#' directory, and collect the results into a [base::data.frame].
+#' compute the outcome measures from each sustained vowel recording , and collect the results into a [base::list].
 #'
 #' @details
 #' The user should be aware that applying this procedure to a directory of sound files may take 9-40 times the total duration of the sound files to perform, depending on the machine and how the application was compiled.
@@ -32,57 +31,62 @@ get_MatlabRuntime <- function(){
 #' The permissive open source licences of these published external toolboxes \insertCite{little2006nonlinear,little2007exploiting,brookes2011voicebox}{superassp} are gratefully acknowledged.
 #' 
 #'
-#' @param directory The directory where sustained vowel samples are stored. The
-#'   directory needs to be writable by the user, since the compiled code will
-#'   also store the results of computations there.
+#' @param listOfFiles A list of input sound files. Currently, the function takes only .wav files.
+#' @param knownLossless A vector of file extensions known to be associated with losslessly encoded speech data. Please note that this parameter is currently not used by the underlying code since only .wav files are processed.
 #'
-#' @return A [tibble::tibble] with one row for each sustained vowel sample,
-#'   and with 340 columns. The first column (`listOfFiles`) contains the file
-#'   names of recordings. The following columns (2 to 340) then contain the
-#'   measurement values for the recording.
+#' @return A named list of 339 voice measurements measurement values for the recording.
 #' @export
 #' 
 #' @references 
 #'   \insertAllCited{}
 #' 
 
-voice_analysis <- function(directory){
+voice_analysis_toolkit <- function(listOfFiles,
+                           knownLossless=c("wav","flac","aiff","aif")){
   
-  directory <- normalizePath(directory)
+  listOfFiles <- normalizePath(listOfFiles)
+  
   currwd <- getwd()
   
-  if(!dir.exists(directory)) stop("The directory '",directory,"' does not extist!")
-  
-  setwd(directory)
-  
-  results <- base::system2(get_VoiceAnalysisToolkitCommand(),
-                c(get_MatlabRuntime(),directory), stderr = TRUE, stdout=TRUE)
-  
-  #voice_analysis_directory_output.mat
-  logger::log_trace(paste(results,sep="\n"))
-  matout <- R.matlab::readMat(file.path(directory,"voice_analysis_directory_output.mat"))
-  
-  #Restore the working directory
-  setwd(currwd)
-  
-  nfiles <- length(purrr::pluck(matout,"va.results"))
-  
-  measurenames <- as.vector(unlist(purrr::pluck(matout,"va.results",1,1,3,1)))
-  
-  d <- matrix(NA, nrow=nfiles,ncol=(1+length(measurenames)))
-  d <- as.data.frame(d)
-  names(d) <- c("listOfFiles",measurenames)
-  
-  for(file in 1:nfiles ){
-    filename <- as.vector(unlist(purrr::pluck(matout,"va.results",file,1,1,1)))
-    values <-  as.vector(unlist(purrr::pluck(matout,"va.results",file,1,2,1)))
-    d[file,1] <- filename
-    d[file,2:(length(measurenames)+1)] <- values
-  }
-  
-  return(tibble::as_tibble(d))
-}
 
+  innerFunction <- function(file){
+    
+    directory <- superassp:::make_dsp_environment()
+    
+    file.copy(from = file,to=directory)
+    setwd(directory)
+    results <- base::system2(get_VoiceAnalysisToolkitCommand(),
+                             c(get_MatlabRuntime(),directory), stderr = TRUE, stdout=TRUE)
+    
+
+    matout <- R.matlab::readMat(file.path(directory,"voice_analysis_directory_output.mat"))
+    #Restore the working directory
+    setwd(currwd)
+    
+    nfiles <- length(purrr::pluck(matout,"va.results"))
+    
+    measurenames <- as.vector(unlist(purrr::pluck(matout,"va.results",1,1,3,1)))
+    values <- as.vector(unlist(purrr::pluck(matout,"va.results",1,1,2,1)))
+    outList <- as.list(values)
+    names(outList) <- measurenames
+    superassp:::clear_dsp_environment(directory)
+    
+    return(outList)
+    
+  }
+
+  purrr::map(.x=listOfFiles,.f=innerFunction)
+
+}
+attr(voice_analysis_toolkit,"ext") <-  "vat" 
+attr(voice_analysis_toolkit,"outputType") <-  "list"
+attr(voice_analysis_toolkit,"nativeFiletypes") <-  c("wav")
+#,"flac","aiff","aif","aifc","au","ogg","opus","mp3","mp4","m4a")
+
+
+
+## Interactive testing
+#voice_analysis(c("~/Desktop/a1.wav","~/Desktop/a2.wav")) -> out
 
 
 
